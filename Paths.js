16bcce760db7253d1c4ca00d6f4f395f506e1a46
Toby Cathcart Burn <tcathcartburn@gmail.com>
2020-12-09 22:37:26 +0000

    Change autofill direction and how paths are scaled

--- Paths.js
+++ Paths.js
@@ -1,3 +1,4 @@
+"use strict";
 function diff([x1,y1],[x2,y2]){
   return [x1-x2,y1-y2]
 }
@@ -27,55 +28,69 @@ function mkzigzag(h,w,scale){
 }
 
 // top edges
-caret =["L",[50,-40],"L",[100,0]]
-point =["L",[40,0], "L",[50,-40], "L",[60,0], "L",[100,0]]
-zigzag = mkzigzag(-40,8,0.7)
-square =["L",[30,0], "L",[30,-40], "L",[70,-40], "L",[70,0], "L",[100,0]]
-curve =["C",[25,25],[75,-90],[100,0]]
-curve2 =["C",[25,25],[25,-70],[50,0],"C",[75,25],[75,-70],[100,0]]
-circ = ["L", [40,0], "A", ["scale",10,10], "0 1 0", [40, 0.001], "L", [100, 0]]
+const caret =["L",[50,-40],"L",[100,0]]
+const point =["L",[40,0], "L",[50,-40], "L",[60,0], "L",[100,0]]
+const zigzag = mkzigzag(-40,8,0.7)
+const square =["L",[30,0], "L",[30,-40], "L",[70,-40], "L",[70,0], "L",[100,0]]
+const curve =["C",[25,25],[75,-90],[100,0]]
+const curve2 =["C",[25,25],[25,-70],[50,0],"C",[75,25],[75,-70],[100,0]]
+const circ = ["L", [40,0], "A", ["scale",10,10], "0 1 0", [40, 0.001], "L", [100, 0]]
 
-shapes=[caret,point,square,curve,curve2,zigzag] //,circ]
+const shapes=[caret,point,square,curve,curve2,zigzag] //,circ]
 
-
-// stretches a path along x and rotates it to flow from start to end.
-// Does not include the point 'start' -  assumes that it was already there
 function makePath(name,start,end){
   if (name=="line"){
     return `L ${end[0]} ${end[1]}`
   }
-  var [dx,dy] = diff(end,start)
-  scale = Math.hypot(dx,dy)
+  return stretchPath(shapes[hash(name)%shapes.length], start, end)
+}
+
+
+// stretches a path along x and rotates it to flow from start to end.
+// Does not include the point 'start' -  assumes that it was already there
+function stretchPath(shape,start,end){
+  if (shape=="line"){
+    return `L ${end[0]} ${end[1]}`
+  }
+  let [dx,dy] = diff(end,start)
+  let scale = Math.hypot(dx,dy)
+  if (scale>10){
+    ;[dx,dy] = mul([dx,dy],10/scale)
+    start = add(mul(start, 0.5+5/scale), mul(end, 0.5-5/scale))
+  }
   dx/=100
   dy/=100
-  pts=[]
-  for (p of shapes[hash(name)%shapes.length]){
+  let pts=["L "+start.join(" ")+" "]
+  for (let p of shape){
     if (Array.isArray(p)){
       if (p[0]==="scale"){
-        var[x,y]=p.slice(1)
-        y=y*13/scale
+        let [x,y] = p.slice(1)
+        y=y*13/Math.min(scale,10)
         pts.push([x*dx+y*dy, x*dy-y*dx].join(" "))
       }else{
-        var [x,y]=p
-        y=y*5/scale
+        let [x,y] = p
+        y = y*5/Math.min(scale,10)
         pts.push(add(start,[x*dx+y*dy, x*dy-y*dx]).join(" "))
       }
     }
     else pts.push(p)
   }
+  if (scale>10){
+    pts.push("L "+end.join(" "))
+  }
   return pts.join(" ")+" "
 }
-MARX=3// Rise up!
-MARY=3
-BASEHEIGHT=7//with text (vertical center is BASEHEIGHT/2)
+const MARX=3// Rise up!
+const MARY=3
+const BASEHEIGHT=7//with text (vertical center is BASEHEIGHT/2)
 
 // Given starting coordinates and a base type, make a path
 function simplePath(x,y,w,h,type){
   y = y-(BASEHEIGHT-h)/2
-  bl = [x-MARX,y+MARY]
-  br = [x+w+MARX,y+MARY]
-  tr = [x+w+MARX,y-h-MARY]
-  tl = [x-MARX,y-h-MARY]
+  let bl = [x-MARX,y+MARY]
+  let br = [x+w+MARX,y+MARY]
+  let tr = [x+w+MARX,y-h-MARY]
+  let tl = [x-MARX,y-h-MARY]
   var pth = "M "+bl[0]+" "+bl[1]+" "+
     makePath(type,bl,br) +
     makePath(type,br,tr) +
@@ -87,13 +102,13 @@ function simplePath(x,y,w,h,type){
 //Note: x doesn't translate the output of this, just gives the width of the thing in the hole
 function filledHolePath(x,y,w,h,type){
   y = y-(BASEHEIGHT-h)/2
-  bl = [-MARX,y+MARY]
-  bm = [x+MARX,y+MARY]
-  br = [x+w+MARX,y+MARY]
-  tr = [x+w+MARX,y-h-MARY]
-  tm = [x+MARX,y-h-MARY]
-  tl = [-MARX,y-h-MARY]
-  var pth = "M "+bl[0]+" "+bl[1]+" "+
+  let bl = [-MARX,y+MARY]
+  let bm = [x+MARX,y+MARY]
+  let br = [x+w+MARX,y+MARY]
+  let tr = [x+w+MARX,y-h-MARY]
+  let tm = [x+MARX,y-h-MARY]
+  let tl = [-MARX,y-h-MARY]
+  let pth = "M "+bl[0]+" "+bl[1]+" "+
     makePath(type,bl,bm) +
     makePath("line",bm,br) +
     makePath("line",br,tr) +
