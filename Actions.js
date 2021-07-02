b11bb82044ceee4db05a1e81b48776cee02eedf2
Toby Cathcart Burn <tcathcartburn@gmail.com>
2021-07-02 01:32:20 +0100

    Replace tabs by spaces

--- Actions.js
+++ Actions.js
@@ -1,9 +1,9 @@
 
 function deleteNode(g){
-	if(!(g.isHole || g.isLHS)){
-		g.delete()
-		return true
-	}
+  if(!(g.isHole || g.isLHS)){
+    g.delete()
+    return true
+  }
 }
 
 function fillOrUnfill(g){
@@ -29,46 +29,46 @@ function fillOrUnfill(g){
 }
 
 function dragInto(argument,hole,pointerPos,pointerDelta,dropEffect){
-	// Don't do anything if the target is not something we expect
-	if (!root.contains(hole)){
-		console.log("Target is not part of a term")
-		return;
-	}
-	if (dropEffect=="move" && argument.contains(hole)){
-		console.log("Cannot move into self")
-		return;
-	}
-	if (isDefn(argument)){
-	  if (hole===root && dropEffect=="move"){
-		  argument.changexy(...pointerDelta)
-		}
-		else console.log("can't copy definitions");
-		return;
-	}
+  // Don't do anything if the target is not something we expect
+  if (!root.contains(hole)){
+    console.log("Target is not part of a term")
+    return;
+  }
+  if (dropEffect=="move" && argument.contains(hole)){
+    console.log("Cannot move into self")
+    return;
+  }
+  if (isDefn(argument)){
+    if (hole===root && dropEffect=="move"){
+      argument.changexy(...pointerDelta)
+    }
+    else console.log("can't copy definitions");
+    return;
+  }
 
-	// Find out how to move the argument into the hole (if appropriate)
-	let cmd = getMoveCommand(argument,hole,pointerPos)
-	if (cmd===false) return; // the attempted move is invalid
+  // Find out how to move the argument into the hole (if appropriate)
+  let cmd = getMoveCommand(argument,hole,pointerPos)
+  if (cmd===false) return; // the attempted move is invalid
 
-	// save some data that may be helpful for repositioning
-	let savedpos = argument.getPageXY()
+  // save some data that may be helpful for repositioning
+  let savedpos = argument.getPageXY()
 
-	if (dropEffect=="copy"){
-		argument=argument.duplicate()
-	}
-	let fn = cmd[0]
-	cmd[0] = argument
-	let moved = fn(...cmd)// moved within the syntax tree
-	console.log(moved)
-	if (argument.parentElement===root){ // this implies moved in the current code
-		argument.setxy(savedpos[0]+pointerDelta[0], savedpos[1]+pointerDelta[1])
-	}
-	if (dropEffect=="copy"){
-		argument.classList.remove("hidden")
-	}
-	else if(!moved){
-		argument.changexy(...pointerDelta)
-	}
+  if (dropEffect=="copy"){
+    argument=argument.duplicate()
+  }
+  let fn = cmd[0]
+  cmd[0] = argument
+  let moved = fn(...cmd)// moved within the syntax tree
+  console.log(moved)
+  if (argument.parentElement===root){ // this implies moved in the current code
+    argument.setxy(savedpos[0]+pointerDelta[0], savedpos[1]+pointerDelta[1])
+  }
+  if (dropEffect=="copy"){
+    argument.classList.remove("hidden")
+  }
+  else if(!moved){
+    argument.changexy(...pointerDelta)
+  }
 
 }
 
@@ -79,7 +79,7 @@ function getMoveCommand(argument,hole,pointerPosition){
     if(isArg(argument)){
       if (argument.parentElement.parentElement===hole){
         // If you drag a filled argument just outside its hole, make it float
-				// (and make it the first in the list of floating terms)
+        // (and make it the first in the list of floating terms)
         return [makeFloating,argument.parentElement,"start"]
       }
     }
@@ -87,7 +87,7 @@ function getMoveCommand(argument,hole,pointerPosition){
       //Let something be made floating by dragging it just outside its context
       return [makeFloating,argument.scope]
     }
-		console.log("Not a hole") // Consider trying the parent hole
+    console.log("Not a hole") // Consider trying the parent hole
     return false
   }
   else if(!argument.scope.contains(hole)){
@@ -95,21 +95,21 @@ function getMoveCommand(argument,hole,pointerPosition){
     // alternatively, consider putting the new context into this hole (if possible)
     return false
   }
-	else if (hole===root){//Put stuff into the root
-		return [makeFloating,hole,"end"]
-	}
+  else if (hole===root){//Put stuff into the root
+    return [makeFloating,hole,"end"]
+  }
   else if(hole.filled===argument){// Take something out of a hole by moving it a little
     return [makeFloating,hole,"start"]
   }
-	else if(hole.filled){//don't put things into holes that are already filled
-		console.log("Already filled")
-		return false
-	}
-	else {
+  else if(hole.filled){//don't put things into holes that are already filled
+    console.log("Already filled")
+    return false
+  }
+  else {
     //If the types match and there's no other reason not to, go ahead and fill the hole
     // (currently does not do so)
     return [tryToFillHole,hole,pointerPosition]
-	}
+  }
 }
 
 // See if a term can fit a hole perfectly, taking into account
